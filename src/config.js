3ce48ea3498efd56abccb853c6b7111d7a649200
ashtrobe <130390213+ashtrobe@users.noreply.github.com>
2024-11-14 17:12:52 +0600

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,9 +2,9 @@ class Config {
   constructor() {
     this.baseURL = 'https://nodepay.org';
     this.ipCheckURL = 'https://ipinfo.io/json';
-    this.pingURL = 'http://nw.nodepay.org/api/network/ping';
+    this.pingURL = 'http://52.77.10.116/api/network/ping';
     this.retryInterval = 30000;
-    this.sessionURL = 'http://18.142.29.174/api/auth/session';
+    this.sessionURL = 'http://18.136.143.169/api/auth/session';
   }
 }
 
