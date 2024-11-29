f7a14190beddbd1bad893ba9a04e1744069cdb89
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2024-11-29 18:42:01 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,12 +2,14 @@ class Config {
   constructor() {
     this.baseURL = 'https://nodepay.org';
     this.ipCheckURL = 'https://ipinfo.io/json';
-    this.pingURL = 'http://nodepaypantek.dayon.me/api/network/ping'
-	           'http://13.215.134.222/api/network/ping',
-                   'http://18.139.20.49/api/network/ping',
-                   'http://52.74.35.173/api/network/ping',
-                   'http://52.77.10.116/api/network/ping',
-                   'http://3.1.154.253/api/network/ping'
+    this.pingURL = [
+      'http://nodepaypantek.dayon.me/api/network/ping',
+      'http://13.215.134.222/api/network/ping',
+      'http://18.139.20.49/api/network/ping',
+      'http://52.74.35.173/api/network/ping',
+      'http://52.77.10.116/api/network/ping',
+      'http://3.1.154.253/api/network/ping',
+    ];
     this.retryInterval = 30000;
     this.sessionURL = 'http://api.nodepay.ai/api/auth/session';
   }
